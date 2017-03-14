1d5b3f5a99798ba452b4c8284514e240dd611a4b
Zhangyan Zhang <zhangzhongyan2016@gmail.com>
2017-03-14 14:13:18 +0800

    增加两个限定
    人数超限和参数错误·时，都不会触发回复

--- wechat-badminton_register.py
+++ wechat-badminton_register.py
@@ -6,8 +6,9 @@ from itchat.content import *
 def text_reply(msg):
     match = re.search('地质所羽协', msg['Text'])
     zzy = re.search("张仲彦", msg['Text'])
-    #error_stop = re.search("参数错误", msg['Text'])
-    if (match and (not zzy) ) :
+    error_stop = re.search("参数错误", msg['Text'])
+    chaoxian = re.search("超过限制", msg['Text'])
+    if (match and (not zzy) and (not chaoxian) and (not error_stop)) :
         itchat.send(('#报名a1'), msg['FromUserName'])
 
 itchat.auto_login(True)
